f4f050602d435e47a5991fb51412713d0dd0b34d
Savelyev Daniil <savelyev.dm@mail.ru>
2017-10-19 13:23:11 +0300

    Button added.

--- app/src/main/java/ru/developsdm/gitaddproject2/SingleActivity.java
+++ app/src/main/java/ru/developsdm/gitaddproject2/SingleActivity.java
@@ -2,6 +2,8 @@ package ru.developsdm.gitaddproject2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SingleActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class SingleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(l -> Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show());
     }
 }
